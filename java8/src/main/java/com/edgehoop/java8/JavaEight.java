ef82cce8c740df94fe6752ab89a5d0d48d43e1e8
bdodia <bdodia@users.noreply.github.com>
2018-12-17 20:29:14 +0000

    Added Date Time section

--- java8/src/main/java/com/edgehoop/java8/JavaEight.java
+++ java8/src/main/java/com/edgehoop/java8/JavaEight.java
@@ -1,8 +1,15 @@
 package com.edgehoop.java8;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JavaEight {
     public static void main(String[] args) {
 
+        //Streams Section
         CountryStatistics countryStatistics = new CountryStatistics();
 
         countryStatistics.addCountry(new Country("AUT","Austria", Continent.EUROPE, 8600000));
@@ -19,6 +26,32 @@ public class JavaEight {
         countryStatistics.threeCountriesWithPopulationsAbove40000000();
         countryStatistics.countriesByContinent();
 
+        //Date Time Section
+        List<String> letters = new ArrayList<>();
+        letters.add("Alpha");
+        letters.add("Beta");
+        letters.add("Gamma");
+        letters.add("Delta");
+
+        Boolean containsZeta = letters.contains("Zeta");
+
+        String removedValue = letters.remove(2);
+
+        System.out.println(letters);
+        System.out.println(containsZeta);
+        System.out.println(removedValue);
+
+        LocalDate today = LocalDate.now();
+        LocalTime todaysTime = LocalTime.of(14,32,45);
+        LocalDateTime specificDataTime = LocalDateTime.of(today,todaysTime);
+
+        System.out.println(today);
+        System.out.println(todaysTime);
+        System.out.println(specificDataTime);
+
+        LocalDate furtureDate = today.plusMonths(4).plusWeeks(2);
+
+        System.out.println(furtureDate);
     }
 }
 
